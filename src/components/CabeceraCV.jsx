d606d76ca62fa151e62ad54c08cc6c6f699a2a10
Pablo_Fuenmayor <fuenmayor0908@hotmail.com>
2025-11-04 15:17:41 -0500

    feat: componente CabeceraCV con datos personales actualizados

---
+++ src/components/CabeceraCV.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CabeceraCV = () => {
+  return (
+    <header>
+      <h1>Pablo Fuenmayor</h1>
+      <h2>Administrador de Empresas</h2>
+      <p>Tel: +57 3016305090 | Email: fuenmayor0908@hotmail.com | Ciudad: Cartagena</p>
+    </header>
+  );
+};
+
+export default CabeceraCV;
